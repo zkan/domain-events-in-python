ca41aae7b7cd4a43ac929569a164f9149c5e9d87
Kan Ouivirach <kan@odds.team>
2024-09-12 06:07:40 +0000

    Rearrange code a bit

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ from user import User
 from user_handlers import dispatcher
 
 
-# Create a user and trigger domain events
-user = User(user_id=1, email="user@example.com", dispatcher=dispatcher)
-user.register()  # This triggers the UserRegistered event and sends the welcome email
-user.login()     # This triggers the UserLoggedIn event and logs the login
+if __name__ == "__main__":
+    # Create a user and trigger domain events
+    user = User(user_id=1, email="user@example.com", dispatcher=dispatcher)
 
-# ------------- Different domain -------------
+    # This triggers the UserRegistered event and sends the welcome email
+    user.register()
+    # This triggers the UserLoggedIn event and logs the login
+    user.login()
 
-import analytics_handlers
+    print(dispatcher._handlers)
+    print("--------------------------")
+
+    # ------------- Different domain -------------
+
+    import analytics_handlers
 
 
-# When a user logs in again, it will dispatch an event to analytics domain
-user.login()
+    # When a user logs in again, it will dispatch an event to analytics domain
+    user.login()
+
+    print(dispatcher._handlers)
